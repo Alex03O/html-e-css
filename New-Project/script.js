fa69b0657597b8ac71bdcac70711f5a1449974f2
Alex03O <89613750+Alex03O@users.noreply.github.com>
2022-03-21 22:41:31 -0300

    Correções

--- New-Project/script.js
+++ New-Project/script.js
@@ -1,9 +1,11 @@
 const btnmobile = document.getElementById('btn-mobile')
 
-function toggleMenu(){
+function toggleMenu(event){
+    if(event.type === 'touchstart') event.preventDefault()
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
 }
 
 
 btnmobile.addEventListener('click', toggleMenu);
+btnmobile.addEventListener('touchstart', toggleMenu);
